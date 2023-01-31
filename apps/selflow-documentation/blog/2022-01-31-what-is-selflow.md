1d75d8fb2862d02f9a7ebeaa672476888a0a7016
Anthony Quéré <47711333+Anthony-Jhoiro@users.noreply.github.com>
2023-01-31 16:15:24 +0100

    :memo: Setup documentation (#12)
    * :memo: Setup docusaurus
    
    * :construction_worker: Configure doc deployment to GitHub pages
    
    * :memo: Fix broken link
    
    * :bug: Fix base path
    
    * :construction_worker: Disable workflow in PRs

---
+++ apps/selflow-documentation/blog/2022-01-31-what-is-selflow.md
@@ -0,0 +1,21 @@
+---
+slug: what-is-selflow
+title: What is Selflow ?
+authors: anthony-quere
+tags: []
+---
+
+Selflow is a simple workflow orchestrator.
+
+Workflows in Selflow are a set of tasks with requirements or none that will run asynchronously in a Docker container
+
+It is currently a school project but I have the ambition to make it grow even after.
+
+It was designed to be runnable on any linux machine in your servers, homelab or in the cloud.
+
+## Easy to use
+
+- Build workflows using Yaml
+- Write your own plugins with [the language you want](https://grpc.io/docs/languages/)
+- Supports Docker and SSH steps
+- See your workflow execution using a simple ui ([wip](2022-01-31-state-of-selflow.md))
---
+++ apps/selflow-documentation/docusaurus.config.js
@@ -0,0 +1,118 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Selflow - Documentation',
+  tagline: 'Orchestrate workflows anywhere you want',
+  url: 'http://localhost:3000',
+  baseUrl: '/selflow/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'selflow', // Usually your GitHub org/user name.
+  projectName: 'selflow', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Selflow',
+        logo: {
+          alt: 'Selflow Logo',
+          src: 'img/selflow-logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentations',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/seflow/selflow',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Selflow, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
