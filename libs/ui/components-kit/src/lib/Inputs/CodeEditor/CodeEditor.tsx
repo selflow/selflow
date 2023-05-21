1d8fce893dc090c366f5d1a65acddae9f07f8450
anthony <quereantho@gmail.com>
2023-05-21 11:42:19 +0200

    :heavy_minus_sign: remove Ace editor

--- libs/ui/components-kit/src/lib/Inputs/CodeEditor/CodeEditor.tsx
+++ libs/ui/components-kit/src/lib/Inputs/CodeEditor/CodeEditor.tsx
@@ -1,20 +1,30 @@
 import { Label } from '../Label/Label';
-import CodeMirror, {ReactCodeMirrorProps} from '@uiw/react-codemirror';
+import CodeMirror, { ReactCodeMirrorProps } from '@uiw/react-codemirror';
 import { StreamLanguage } from '@codemirror/language';
-import {shell} from "@codemirror/legacy-modes/mode/shell"
+import { shell } from '@codemirror/legacy-modes/mode/shell';
 
-
-export type CodeEditorProps = Omit<ReactCodeMirrorProps, 'theme' | 'setOptions'> & {
+export type CodeEditorProps = Omit<
+  ReactCodeMirrorProps,
+  'theme' | 'setOptions'
+> & {
   lang: 'sh';
   label: string;
 };
 
-export const CodeEditor = ({ lang, label, ...editorProps }: CodeEditorProps) => {
+export const CodeEditor = ({
+  lang,
+  label,
+  ...editorProps
+}: CodeEditorProps) => {
   return (
     <div className={'my-2'}>
       <Label>
         <span>{label}</span>
-        <CodeMirror height="200px" extensions={[StreamLanguage.define(shell)]} {...editorProps} />
+        <CodeMirror
+          height="200px"
+          extensions={[StreamLanguage.define(shell)]}
+          {...editorProps}
+        />
         {/*<AceEditor*/}
         {/*  className={'rounded-lg w-full h-64'}*/}
         {/*  mode={lang}*/}
--- libs/ui/components-kit/src/lib/Inputs/index.tsx
+++ libs/ui/components-kit/src/lib/Inputs/index.tsx
@@ -3,4 +3,4 @@ import dynamic from 'next/dynamic';
 export * from './Input/Inputs';
 export * from './Label/Label';
 export * from './MultiSelect/MultiSelect';
-export * from './CodeEditor/CodeEditor'
+export * from './CodeEditor/CodeEditor';
