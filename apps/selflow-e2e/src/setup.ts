34d6c49382349d2ad35e18d17738a5293b90b4a9
anthony <quereantho@gmail.com>
2023-10-22 11:03:42 +0200

    :green_heart: fix nx recreate-daemon dependency tree

--- apps/selflow-e2e/src/setup.ts
+++ apps/selflow-e2e/src/setup.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process';
 
 export function setup() {
-  execSync(`go run github.com/selflow/selflow/cmd/selflow-cli recreate-daemon`);
+  //execSync(`go run github.com/selflow/selflow/cmd/selflow-cli recreate-daemon`);
 }
