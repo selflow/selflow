b0a620ff0385b3c6a5fde508f2ccb1a38a3d577b
Anthony Quéré <47711333+Anthony-Jhoiro@users.noreply.github.com>
2023-12-15 21:13:40 +0100

    :arrow_up: Upgrade NX version to 17 (#73)
    * :arrow_up: Upgrade NX version
    
    * :rotating_light: Fix linter issues

--- libs/selflow-daemon-clients/selflow-node-client/vite.config.ts
+++ libs/selflow-daemon-clients/selflow-node-client/vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import dts from 'vite-plugin-dts';
 import { join } from 'path';
 
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/selflow-node-client',
 
   plugins: [
@@ -15,9 +16,7 @@ export default defineConfig({
       skipDiagnostics: true,
     }),
 
-    viteTsConfigPaths({
-      root: '../../',
-    }),
+    nxViteTsPaths(),
   ],
 
   // Uncomment this if you are using workers.
@@ -32,6 +31,9 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    outDir: '../../../dist/libs/selflow-daemon-clients/selflow-node-client',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
@@ -48,6 +50,12 @@ export default defineConfig({
   },
 
   test: {
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory:
+        '../../coverage/libs/selflow-daemon-clients/selflow-node-client',
+      provider: 'v8',
+    },
     globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
--- libs/ui/components-kit/src/lib/Inputs/index.tsx
+++ libs/ui/components-kit/src/lib/Inputs/index.tsx
@@ -1,5 +1,3 @@
-import dynamic from 'next/dynamic';
-
 export * from './Input/Inputs';
 export * from './Label/Label';
 export * from './MultiSelect/MultiSelect';
