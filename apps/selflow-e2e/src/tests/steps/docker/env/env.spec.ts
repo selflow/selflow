ff0824eca59f67661933f7ca02cf86d60e0cd262
Anthony Quéré <47711333+Anthony-Jhoiro@users.noreply.github.com>
2023-12-21 17:57:14 +0100

    :sparkles: add environment variables for docker containers (#80)
    * :sparkles: add environment variables for docker containers
    
    * :art: format code
    
    * :white_check_mark: Add test for dynamic docker environment variables
    
    * :white_check_mark: Format changes
    
    * :wrench: Fix repository for releases (#82)
    
    * :wrench: Remove spcec.ts from releaser (#81)
    
    * :pencil2: Fix typo

---
+++ apps/selflow-e2e/src/tests/steps/docker/env/env.spec.ts
@@ -0,0 +1,69 @@
+import { startCliRun, startRun } from '../../../../tools/run';
+import { expect, test } from 'vitest';
+import { join } from 'path';
+import { parseLogs } from '../../../../tools/logParser';
+import { matchers } from '../../../../tools/trace';
+
+expect.extend(matchers);
+
+describe('Environment variables', function () {
+  describe('step-a and step-b should prompt the values of their environment variables ', function () {
+    const configFilePath = join(__dirname, 'env.yaml');
+
+    const verifyLogs = (logs: string) => {
+      const trace = parseLogs(logs);
+
+      expect(logs).not.toEqual('');
+
+      expect(trace).toHaveStep('step-a');
+      expect(trace).toHaveStep('step-b');
+
+      expect(trace).toHaveStepTerminatedWithStatus(['step-a', 'SUCCESS']);
+      expect(trace).toHaveStepTerminatedWithStatus(['step-b', 'SUCCESS']);
+
+      expect(trace).toHaveStepLogged(['step-a', '##step-a##']);
+      expect(trace).toHaveStepLogged(['step-b', '##step-b##']);
+    };
+
+    test('selflow-daemon', async () => {
+      const logs = await startRun(configFilePath);
+      verifyLogs(logs);
+    });
+
+    test('selflow-cli', async () => {
+      const logs = await startCliRun(configFilePath);
+      verifyLogs(logs);
+    });
+  });
+
+  describe('dynamic docker environment variables', function () {
+    const configFilePath = join(__dirname, 'env-dynamic.yaml');
+
+    const verifyLogs = (logs: string) => {
+      const trace = parseLogs(logs);
+
+      expect(logs).not.toEqual('');
+
+      expect(trace).toHaveStep('step-a');
+      expect(trace).toHaveStep('step-b');
+      expect(trace).toHaveStep('step-c');
+
+      expect(trace).toHaveStepTerminatedWithStatus(['step-a', 'SUCCESS']);
+      expect(trace).toHaveStepTerminatedWithStatus(['step-b', 'SUCCESS']);
+      expect(trace).toHaveStepTerminatedWithStatus(['step-c', 'SUCCESS']);
+
+      expect(trace).toHaveStepLogged(['step-a', 'my-env-value']);
+      expect(trace).toHaveStepLogged(['step-c', 'bar']);
+    };
+
+    test('selflow-daemon', async () => {
+      const logs = await startRun(configFilePath);
+      verifyLogs(logs);
+    });
+
+    test('selflow-cli', async () => {
+      const logs = await startCliRun(configFilePath);
+      verifyLogs(logs);
+    });
+  });
+});
