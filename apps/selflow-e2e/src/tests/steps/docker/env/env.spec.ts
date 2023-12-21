b317d2196e59ba51148a6037411cb9b70c2d7383
anthony <quereantho@gmail.com>
2023-12-21 17:10:05 +0100

    :white_check_mark: Add test for dynamic docker environment variables

--- apps/selflow-e2e/src/tests/steps/docker/env/env.spec.ts
+++ apps/selflow-e2e/src/tests/steps/docker/env/env.spec.ts
@@ -35,4 +35,35 @@ describe('Environment variables', function () {
       verifyLogs(logs);
     });
   });
+
+  describe('Dynamic docker environment variables', function () {
+    const configFilePath = join(__dirname, 'env-dynamic.yaml');
+
+    const verifyLogs = (logs: string) => {
+      const trace = parseLogs(logs);
+
+      expect(logs).not.toEqual('');
+
+      expect(trace).toHaveStep('step-a');
+      expect(trace).toHaveStep('step-b');
+      expect(trace).toHaveStep('step-c');
+
+      expect(trace).toHaveStepTerminatedWithStatus(['step-a', 'SUCCESS']);
+      expect(trace).toHaveStepTerminatedWithStatus(['step-b', 'SUCCESS']);
+      expect(trace).toHaveStepTerminatedWithStatus(['step-c', 'SUCCESS']);
+
+      expect(trace).toHaveStepLogged(['step-a', 'my-env-value']);
+      expect(trace).toHaveStepLogged(['step-c', 'bar']);
+    };
+
+    test('selflow-daemon', async () => {
+      const logs = await startRun(configFilePath);
+      verifyLogs(logs);
+    });
+
+    test('selflow-cli', async () => {
+      const logs = await startCliRun(configFilePath);
+      verifyLogs(logs);
+    });
+  });
 });
